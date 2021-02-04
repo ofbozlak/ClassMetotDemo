9802292f924cd9743f190e3e642a00115f382553
Ömer_faruk <faruk666bozlak@gmail.com>
2021-02-04 12:39:29 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi.");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("İsim Güncellendi !");
+        }
+        public void Update2(Customer customer)
+        {
+            Console.WriteLine("Şirket Güncellendi !");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Ömer Faruk";
+            customer1.LastName = "Bozlak";
+            customer1.Company = "Öğrenci";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer1);
+            customerManager.Update(customer1);
+
+
+        }
+    }
+}
